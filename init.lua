8cc6e95159470a359c5e3f4c571e5be5b9b6cfc0
Yanuar Arifin <yanuar.arifin@spesolution.com>
2023-05-21 10:14:48 +0700

    feat: lualine use full filepath relative to root dir

--- init.lua
+++ init.lua
@@ -130,6 +130,14 @@ require('lazy').setup({
         component_separators = '|',
         section_separators = '',
       },
+      sections = {
+        lualine_c = {
+          {
+            'filename',
+            path = 1, -- make file path full to relative root
+          }
+        }
+      }
     },
   },
 
