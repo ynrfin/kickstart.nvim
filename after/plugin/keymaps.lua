6d77637119830818a698fe964f7656caadc0b3fe
Yanuar Arifin <yanuar.arifin@spesolution.com>
2024-01-29 16:09:44 +0700

    feat: add mapping to open  & navigate diagnostic

--- after/plugin/keymaps.lua
+++ after/plugin/keymaps.lua
@@ -7,3 +7,7 @@ vim.api.nvim_set_keymap('n', '<F8>', ':DBUIToggle<CR>', {noremap=true, silent=tr
 -- (which should be regularly)
 -- check if working on other 
 vim.api.nvim_set_keymap('n', '<leader><leader>s', ':source C:/Users/Asani/AppData/Local/nvim/after/plugin/snippets.lua <CR>', {noremap=true, silent=true})
+-- vim.api.nvim_set_keymap("n", "<leader>do", vim.diagnostic.open_float, {noremap=true, silent=true})
+vim.api.nvim_set_keymap("n", "<leader>do", ":lua vim.diagnostic.open_float(nil, {focus=false, scope='cursor'})<CR>", {noremap=true, silent=true})
+vim.api.nvim_set_keymap("n", "<leader>dn", ":lua vim.diagnostic.goto_next()(nil, {focus=false, scope='cursor'})<CR>", {noremap=true, silent=true})
+vim.api.nvim_set_keymap("n", "<leader>dp", ":lua vim.diagnostic.goto_prev()(nil, {focus=false, scope='cursor'})<CR>", {noremap=true, silent=true})
