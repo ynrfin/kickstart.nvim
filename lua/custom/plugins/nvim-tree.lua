8a0b65b940accd509439fb2404452eb2c4112291
Yanuar Arifin <yanuar.arifin@spesolution.com>
2024-02-15 09:28:15 +0700

    feat: sync nvim-tree when cwd changes

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -2,6 +2,11 @@
 return {
   "nvim-tree/nvim-tree.lua",
   config = function()
-    require("nvim-tree").setup {}
+    require("nvim-tree").setup {
+      update_focused_file = {
+        update_cwd = true,
+      },
+      sync_root_with_cwd = true
+    }
   end,
 }
