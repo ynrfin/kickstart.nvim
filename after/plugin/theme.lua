81dee77599299b7d05503afd8052cd438ab6680e
Yanuar Arifin <yanuar.arifin@spesolution.com>
2024-03-26 08:44:10 +0700

    fix: set theme

--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -1,3 +1,6 @@
 -- vim.cmd [[colorscheme neon]]
 -- vim.cmd [[colorscheme tokyonight-moon]]
 -- vim.cmd [[colorscheme nordfox]]
+vim.cmd [[colorscheme nightfox]]
+-- vim.cmd [[colorscheme catpuccin]]
+-- vim.cmd [[colorscheme kanagawa]]
