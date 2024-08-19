193984a68c8a2a6780579358cbaee6b49fa7febb
Yanuar Arifin <3326885+ynrfin@users.noreply.github.com>
2024-08-19 15:43:59 +0700

    fix: remove phpactor. only show errors without anything useful

--- init.lua
+++ init.lua
@@ -429,7 +429,7 @@ vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = 
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python','javascript', 'bash', 'php', 'html', 'css' },
+    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python','javascript', 'bash', 'html', 'css' },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
