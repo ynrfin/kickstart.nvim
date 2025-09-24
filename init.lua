e75623a75257604dbbb7d1d4581ac6b69c1a1b21
Yanuar Arifin <3326885+ynrfin@users.noreply.github.com>
2025-09-24 14:46:27 +0700

    feat: add buffer source for completion. so blink can recommend text on buffer

--- init.lua
+++ init.lua
@@ -880,7 +880,7 @@ require('lazy').setup({
       },
 
       sources = {
-        default = { 'lsp', 'path', 'snippets', 'lazydev' },
+        default = { 'lsp', 'path', 'snippets', 'lazydev', 'buffer' },
         providers = {
           lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
         },
