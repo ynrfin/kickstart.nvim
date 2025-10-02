d69c675dd578d75cd8fa691bc159493b1784c85a
Yanuar Arifin <3326885+ynrfin@users.noreply.github.com>
2025-10-02 10:07:05 +0700

    feat: disable lint aftre write post, it make go file janky with indent and auto new line

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -43,18 +43,18 @@ return {
 
       -- Create autocommand which carries out the actual linting
       -- on the specified events.
-      local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
-      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
-        group = lint_augroup,
-        callback = function()
-          -- Only run the linter in buffers that you can modify in order to
-          -- avoid superfluous noise, notably within the handy LSP pop-ups that
-          -- describe the hovered symbol using Markdown.
-          if vim.bo.modifiable then
-            lint.try_lint()
-          end
-        end,
-      })
+      -- local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+      -- vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+      --   group = lint_augroup,
+      --   callback = function()
+      --     -- Only run the linter in buffers that you can modify in order to
+      --     -- avoid superfluous noise, notably within the handy LSP pop-ups that
+      --     -- describe the hovered symbol using Markdown.
+      --     if vim.bo.modifiable then
+      --       lint.try_lint()
+      --     end
+      --   end,
+      -- })
     end,
   },
 }
